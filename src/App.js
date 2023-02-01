a164c925f00273e7fcb7921cfc92208949097366
willzer0 <pembunuh4444@gmail.com>
2023-02-01 21:31:31 +0700

    henshinv2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Myarticle from "../src/pages/Myarticle";
 import Login from "./pages/Login";
-import Edit from "./pages/EditComments";
 import EditComments from "./pages/EditComments";
 import Editpage from "./pages/Editpage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Editpage />} />
+        <Route path="/my-article.html" element={<Myarticle />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/comments" element={<EditComments />} />
+        <Route path="/admin/comments/edit" element={<Editpage />} />
       </Routes>
     </BrowserRouter>
   );
